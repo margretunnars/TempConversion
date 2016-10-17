69d30115b253e1685b5a60c2eda85e9a17169f6a
Margrét Unnarsdóttir <margretunnarsdottir@Margrets-MacBook-Pro.local>
2016-10-17 11:07:22 +0100

    first commit

---
+++ TempConv/TempConversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TConv
+{
+	class TempConv
+	{
+		public static void Main(string[] args)
+		{
+			double fahrTemp, celTemp;
+			string input;
+
+			Console.WriteLine("Enter a temperature in decrees Celsius");
+			input = Console.ReadLine();
+			celTemp = double.Parse(input);
+
+			fahrTemp = (celTemp + 32.0) * 9.0 / 5.0;
+
+			Console.WriteLine("The temperature in degrees is " + fahrTemp + " in degrees Fahrenheit");
+
+
+
+		}
+	}
+}
